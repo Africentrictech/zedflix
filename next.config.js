fe4ef7d99a664bef6807d3cadcba60fd5ece8ff2
africentrictechnologiesltd <africentrictechnologiesltd@gmail.com>
2025-10-08 20:36:35 +0000

    Fix login page background image and build issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "javascript/auto",
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/wasm",
+          outputPath: "static/wasm",
+        },
+      },
+    });
+
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("brotli-dec-wasm");
+    }
+
+    return config;
   },
 }
--- pages/search.tsx
+++ pages/search.tsx
@@ -5,7 +5,7 @@ const Layout = dynamic(import('../components/Layout'));
 export default function Search() {
     return (
         <Layout>
-            
+            <h1>Search</h1>
         </Layout>
     )
 }
